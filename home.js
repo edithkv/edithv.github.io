abf252a146f000246c547d8f3bb242e054ed8fe4
edithv <55645700+edithv@users.noreply.github.com>
2019-11-13 18:40:39 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
+
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
   var dataText = [ "welcome to my space."];
